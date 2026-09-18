5e5339121784bed6ade8dbb008ac6cacbef1569f
PavelSBvt <SPS260951@yandex.ru>
2026-09-18 23:09:40 +0300

    > [java] create JScrollPanel with content

--- src/main/java/ru/sablin/lab2/Main.java
+++ src/main/java/ru/sablin/lab2/Main.java
@@ -10,9 +10,18 @@ public class Main {
 
         BMI form = new BMI();
         frame.setContentPane(form.getContentPane());
+        JPanel content = form.getContentPane();
 
-        frame.pack();
-        frame.setMinimumSize(new java.awt.Dimension(400, 1000));
+        JScrollPane scroll = new JScrollPane(content);
+
+        scroll.setBorder(null);
+        scroll.getVerticalScrollBar().setUnitIncrement(16);
+        scroll.getHorizontalScrollBar().setUnitIncrement(16);
+
+        frame.setContentPane(scroll);
+
+        frame.setSize(900, 700);
+        frame.setMinimumSize(new java.awt.Dimension(500, 400));
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
